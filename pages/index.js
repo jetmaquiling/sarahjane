604a8612104853f4c6ee8e83711a867d7b6c6908
jetmaquiling <jet.maquiling@gmail.com>
2024-12-12 15:14:14 +0800

    Fixed UI

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,7 @@ const Home = () => {
   const router = useRouter();
   const [nameCall, setNameCall] = useState("Sarah");
   const [loading, setLoading] = useState(true);
+  const [imageLoading, setImageLoading] = useState(false);
   const [imageURL, setImageURL] = useState('');
   const [formData, setFormData] = useState({
     name: '',
@@ -73,22 +74,22 @@ const Home = () => {
   };
 
   const registerUser = async () => {
-    setLoading(true);
+    setImageLoading(true);
     console.log("Registering User", formData);
     const response = await register(formData.name, formData.oneWord, formData.image, formData.inspire, formData.playlist, formData.message, formData.score);
     console.log(response)
     if (response) {
       proceedToNextSlide();
-      setLoading(false);
+      setImageLoading(false);
     } else {
       router.push('/error');
     }
-    setLoading(false);
+    setImageLoading(false);
   }
 
   const handleImageUploadChange = async (event) => {
     try {
-      setLoading(true);
+      setImageLoading(true);
       console.log("Image Upload", event.target.files[0]);
       const file = event.target.files[0];
 
@@ -118,7 +119,7 @@ const Home = () => {
       console.error("Error Upload Image", error);
       // Handle error if needed
     } finally {
-      setLoading(false);
+      setImageLoading(false);
     }
   };
 
@@ -179,12 +180,21 @@ const Home = () => {
         </div>
       )}
 
+      {imageLoading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50 z-50">
+          <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+          </svg>
+        </div>
+      )}
+
 
 
       <div className='h-full flex-grow flex flex-col justify-center max-w-md m-auto border-2 p-5'>
-        <img src="/logo.png" alt="Picture of the Celebrant" className="w-1/2 m-auto" />
+        <img src="/logo.png" alt="Picture of the Celebrant" className="w-1/3 m-auto" />
         <button onClick={proceedToPrevSlide}><ArrowLeft className="h-10 absolute top-5 left-5" /></button>
-        <div className='w-full h-full flex-col max-screen-xl sm:p-10'>
+        <div className='w-full h-full flex-col max-screen-xl p-5'>
           <Swiper
             ref={swiperRef}
             spaceBetween={50}
@@ -193,8 +203,8 @@ const Home = () => {
           >
             <SwiperSlide>
               <div className="w-full flex flex-col items-center">
-                <img src="/image_one.png" alt="Picture of the Celebrant" className="w-4/5 mt-5" />
-                <h2 className="text-xl my-5">Welcome</h2>
+                <img src="/image_one.png" alt="Picture of the Celebrant" className="w-3/5" />
+                <h2 className="text-xl my-2">Welcome</h2>
                 <p className="text-justify font-sans mb-5">This special site is dedicated to the amazing woman who has brought so much love, joy, and inspiration into our lives. Whether you're family, a dear friend, or someone whose life she’s touched, this is your place to celebrate her in a meaningful and fun way.</p>
                 <p className="text-justify font-sans mb-5">
                   Explore the questionnaire, share heartfelt stories, and leave a message that will make her day unforgettable. Let’s fill this space with the warmth, laughter, and love that she so effortlessly gives to everyone around her.</p>
